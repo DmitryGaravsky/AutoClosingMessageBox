7aa52833509a3acb191cd95a3cadffd0f1749552
DmitryGaravsky <garavskydmitry@gmail.com>
2017-11-03 00:19:23 +0300

    New API added
    1) New Show(IWin32Owner) API to support most popular scenarios (in the
    context of #1 )
    2) New Factory() API to provide full control on MessageBox showing

--- AutoClosingMessageBox/AutoClosingMessageBox.cs
+++ AutoClosingMessageBox/AutoClosingMessageBox.cs
@@ -5,12 +5,12 @@
     public class AutoClosingMessageBox {
         readonly string caption;
         readonly DialogResult result;
-        AutoClosingMessageBox(string text, string caption, int timeout, 
+        AutoClosingMessageBox(string caption, int timeout, Func<string, MessageBoxButtons, DialogResult> showMethod,
             MessageBoxButtons buttons = MessageBoxButtons.OK, DialogResult defaultResult = DialogResult.None) {
-            this.caption = caption;
+            this.caption = caption ?? string.Empty;
             this.result = buttons.ToDialogResult(defaultResult);
-            using(new System.Threading.Timer(OnTimerElapsed, result.ToDialogButtonId(buttons), timeout, Timeout.Infinite))
-                this.result = MessageBox.Show(text, caption, buttons);
+            using(new Threading.Timer(OnTimerElapsed, result.ToDialogButtonId(buttons), timeout, Timeout.Infinite))
+                this.result = showMethod(this.caption, buttons);
         }
         void OnTimerElapsed(object state) {
             CloseMessageBoxWindow((int)state);
@@ -19,11 +19,51 @@
             IntPtr hWndMsgBox = Utils.Win32Api.FindMessageBox(caption);
             Utils.Win32Api.SendCommandToDlgButton(hWndMsgBox, dlgButtonId);
         }
-        #region API
+        #region Show API
         public static DialogResult Show(string text,
             string caption = null, int timeout = 1000,
             MessageBoxButtons buttons = MessageBoxButtons.OK, DialogResult defaultResult = DialogResult.None) {
-            return new AutoClosingMessageBox(text, caption ?? string.Empty, timeout, buttons, defaultResult).result;
+            return new AutoClosingMessageBox(caption, timeout,
+                            (capt, btns) => MessageBox.Show(text, capt, btns),
+                        buttons, defaultResult
+                    ).result;
+        }
+        public static DialogResult Show(IWin32Window owner, string text,
+            string caption = null, int timeout = 1000,
+            MessageBoxButtons buttons = MessageBoxButtons.OK, DialogResult defaultResult = DialogResult.None) {
+            return new AutoClosingMessageBox(caption, timeout,
+                            (capt, btns) => MessageBox.Show(owner, text, capt, btns),
+                        buttons, defaultResult
+                    ).result;
+        }
+        #endregion
+        #region Factory
+        public interface IAutoClosingMessageBox {
+            DialogResult Show(
+                    int timeout = 1000,
+                    MessageBoxButtons buttons = MessageBoxButtons.OK,
+                    DialogResult defaultResult = DialogResult.None
+                );
+        }
+        public static IAutoClosingMessageBox Factory(
+            Func<string, MessageBoxButtons, DialogResult> showMethod, string caption = null) {
+            if(showMethod == null)
+                throw new ArgumentNullException(nameof(showMethod));
+            return new Impl(showMethod, caption);
+        }
+        #endregion
+        #region IAutoClosingMessageBox
+        sealed class Impl : IAutoClosingMessageBox {
+            readonly Func<int, MessageBoxButtons, DialogResult, DialogResult> getResult;
+            internal Impl(
+                Func<string, MessageBoxButtons, DialogResult> showMethod, string caption) {
+                this.getResult = (timeout, buttons, defaultResult) =>
+                    new AutoClosingMessageBox(caption, timeout, showMethod, buttons, defaultResult).result;
+            }
+            DialogResult IAutoClosingMessageBox.Show(
+                int timeout, MessageBoxButtons buttons, DialogResult defaultResult) {
+                return getResult(timeout, buttons, defaultResult);
+            }
         }
         #endregion
     }
