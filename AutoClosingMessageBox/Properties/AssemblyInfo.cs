a8a8c443f50ac8e2140d6203b6c18eb378fcc5e1
Sandro Marques <sandro.marques@cartrack.com>
2025-06-24 10:48:40 +0100

    Bump to v1.0.0.7

--- AutoClosingMessageBox/Properties/AssemblyInfo.cs
+++ AutoClosingMessageBox/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyVersion("1.0.0.7")]
 [assembly: CLSCompliant(true)]
 [assembly: AssemblyTitle("AutoClosingMessageBox")]
 [assembly: AssemblyDescription("MessageBox with auto-closing functionality")]
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Microsoft® .NET Framework Extension for Windows Forms")]
 #endif
 [assembly: AssemblyCopyright("Dmitry Garavsky, ©2024")]
-[assembly: AssemblyFileVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.7")]
 [assembly: StringFreezing]
 [assembly: ComVisible(false)]
